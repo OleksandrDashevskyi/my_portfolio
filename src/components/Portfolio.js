d4d7ff72c13872c5884630271d765a63dffe4412
Oleksandr Dashevskyi <dashevskyi2013@gmail.com>
2020-12-27 01:12:42 -0800

    completed resume component and work on portfolio component

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -49,26 +49,25 @@ const useStyles = makeStyles((theme) => ({
 
 const Portfolio = () => {
   const classes = useStyles();
- 
- 
+
   const [localCodingExpanded, setLocalCodingExpanded] = useState(false);
   const [compotExpanded, setCompotExpanded] = useState(false);
 
-  const handleExpandClick1 = () => { 
+  const handleExpandClick1 = () => {
     setLocalCodingExpanded(!localCodingExpanded);
   };
 
-  const handleExpandClick2 = () => { 
-    setCompotExpanded(!compotExpanded);;
- };
+  const handleExpandClick2 = () => {
+    setCompotExpanded(!compotExpanded);
+  };
 
   return (
-    <Box component="div" className={classes.mainContainer} >
+    <Box component="div" className={classes.mainContainer}>
       <Navbar />
       <Grid container justify="center">
         {/* Local Coding */}
         <Grid item xs={12} sm={8} md={6}>
-          <Card className={classes.cardContainer} >
+          <Card className={classes.cardContainer}>
             <CardActionArea>
               <CardMedia
                 component="img"
@@ -99,20 +98,21 @@ const Portfolio = () => {
                   color="textSecondary"
                   component="p"
                 >
-                  loremds dsdssdsd sdsdsds dsdsd sdsd sd sdsd sd sd sds ss s s
-                  sdssd ssdsdsd sds s dsd sd sdsdsd sds s sd sdsd sd sd sds dsds
-                  dsd sd sdsd s sd sd sd sds dsdsss sd sd sd s s dsdss sd sd s
-                  sd sdsdsd s sdsd sd s.
+                  Educational project for IT Academy. The platform built to
+                  teach QA Engineers and Front End Developers programming
+                  lanuages such as JavaScript and Python
                 </Typography>
               </CardContent>
             </CardActionArea>
             <CardActions>
-              <Button size="small" color="primary">
-                Share
+              <Button
+                size="small"
+                color="primary"
+                href="https://localcoding.us/"
+              >
+                Website
               </Button>
-              <Button size="small" color="primary">
-                Learn More
-              </Button>
+              <Button disabled>Learn More</Button>
               <IconButton
                 className={clsx(classes.expand, {
                   [classes.expandOpen]: localCodingExpanded,
@@ -125,35 +125,29 @@ const Portfolio = () => {
               </IconButton>
             </CardActions>
             <Collapse in={localCodingExpanded} timeout="auto" unmountOnExit>
-              <CardContent >
-                <Typography paragraph>Method:</Typography>
+              <CardContent>
                 <Typography paragraph>
-                  Heat 1/2 cup of the broth in a pot until simmering, add
-                  saffron and set aside for 10 minutes.
+                  Web application built for students to learn JavaScript and
+                  Python programming languages. The application consists of
+                  video training material and practical tasks (Data Structure)
+                  on JavaScript and Python languages. Progress monitor developed
+                  to assist students to monitor progress on every task done The
+                  application developed used: React / Redux-Saga / TypeScript on
+                  Frond End Side (UMI JS framework) and Nodejs (Express) on Back
+                  End Side. DataBase use: MongoDB. Deployed; front end - Heroku
+                  and server on Amazon.)
+                </Typography>
+                <Typography variant="h6">
+                  <strong>My accomplishment</strong>
                 </Typography>
                 <Typography paragraph>
-                  Heat oil in a (14- to 16-inch) paella pan or a large, deep
-                  skillet over medium-high heat. Add chicken, shrimp and
-                  chorizo, and cook, stirring occasionally until lightly
-                  browned, 6 to 8 minutes. Transfer shrimp to a large plate and
-                  set aside, leaving chicken and chorizo in the pan. Add
-                  pimentón, bay leaves, garlic, tomatoes, onion, salt and
-                  pepper, and cook, stirring often until thickened and fragrant,
-                  about 10 minutes. Add saffron broth and remaining 4 1/2 cups
-                  chicken broth; bring to a boil.
-                </Typography>
-                <Typography paragraph>
-                  Add rice and stir very gently to distribute. Top with
-                  artichokes and peppers, and cook without stirring, until most
-                  of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-                  medium-low, add reserved shrimp and mussels, tucking them down
-                  into the rice, and cook again without stirring, until mussels
-                  have opened and rice is just tender, 5 to 7 minutes more.
-                  (Discard any mussels that don’t open.)
-                </Typography>
-                <Typography>
-                  Set aside off of the heat to let rest for 10 minutes, and then
-                  serve.
+                  I developed a user interface module named as Progress monitor.
+                  With the module, the student can tract own progress with
+                  progress graph and adding daily reports. So, a teacher can tract
+                  student progress and assist if necessary. After the
+                  implementation of the module, increase positive feedback from
+                  customers and increase the quantity of new and potential
+                  customers (therefore increase the revenue of the company)
                 </Typography>
               </CardContent>
             </Collapse>
@@ -199,12 +193,14 @@ const Portfolio = () => {
               </CardContent>
             </CardActionArea>
             <CardActions>
-              <Button size="small" color="primary">
-                Share
+            <Button
+                size="small"
+                color="primary"
+                href="https://kompot.us/"
+              >
+                Website
               </Button>
-              <Button size="small" color="primary">
-                Learn More
-              </Button>
+              <Button disabled>Learn More</Button>
               <IconButton
                 className={clsx(classes.expand, {
                   [classes.expandOpen]: compotExpanded,
@@ -216,7 +212,7 @@ const Portfolio = () => {
                 <ExpandMoreIcon />
               </IconButton>
             </CardActions>
-            <Collapse in={compotExpanded} timeout="auto" unmountOnExit >
+            <Collapse in={compotExpanded} timeout="auto" unmountOnExit>
               <CardContent>
                 <Typography paragraph>Method:</Typography>
                 <Typography paragraph>
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,11 +1,21 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Typography, Box, Container } from "@material-ui/core";
+import { Typography, Box, Container, Button } from "@material-ui/core";
 import Navbar from "./Navbar";
+import MyResume from "../files/Resume.pdf";
 
 const useStyles = makeStyles((theme) => ({
   mainContainer: {
     background: "#233",
+    boxShadow:
+      "0px 3px 1px -2px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 1px 5px 0px rgba(0,0,0,0.12)",
+  },
+  secondaryContainer: {
+    width: "100%",
+    maxWidth: 800,
+    align: "center",
+    marginTop: "1rem",
+    marginBottom: "2rem",
   },
   timeLine: {
     position: "relative",
@@ -93,102 +103,170 @@ const useStyles = makeStyles((theme) => ({
   },
   heading: {
     color: "tomato",
-    padding: "3rem 0",
+    padding: "1rem 0",
     textTransform: "uppercase",
   },
   subHeading: {
     color: "white",
     padding: "0",
+    marginTop: "2rem",
     textTransform: "uppercase",
+  },
+  button: {
+    marginTop: "1rem",
+    color: "tomato",
+    borderColor: "tomato",
+    background: "#233",
+    textTransform: "none",
   },
 }));
 
 const Resume = () => {
   const classes = useStyles();
+
   return (
     <>
       <Navbar />
-      <Container>
-        <Box component="header" className={classes.mainContainer}>
-          <Box component="div">
-            <Typography variant="h5" align="center" style={{ color: "tomato" }}>
-              SUMMARY
-            </Typography>
-            <Typography variant="body1" align="center" style={{ color: "tan" }}>
-              • React / JavaScript Developer with 3+ years of hands-on
-              experience in application development
-              <br />
-              • Experience in all phases of software life cycle including
-              requirements gathering, project planning development
-              <br />
-              • Strong knowledge of software development life cycle
-              methodologies such as Agile / SCRUM
-              <br />• Experience with Git, continuous integration / development
-            </Typography>
-          </Box>
-          <br />
-          <Box component="div">
-            <Typography variant="h5" align="center" style={{ color: "tomato" }}>
-              TECHNICAL SKILLS
-            </Typography>
-            <Typography
-              variant="subtitle1"
-              align="center"
-              style={{ color: "tan" }}
-            >
-              <strong>Languages:</strong>&nbsp;JavaScript (ES5, ES6),
-              TypeScript, HTML, CSS, JSON
-            </Typography>
-            <Typography
-              variant="subtitle1"
-              align="center"
-              style={{ color: "tan" }}
-            >
-              <strong>Libraries:</strong>&nbsp;React, Redux, Bootstrap, Ant
-              Design, Material UI
-            </Typography>
-            <Typography
-              variant="subtitle1"
-              align="center"
-              style={{ color: "tan" }}
-            >
-              <strong>Testing Tools:</strong>&nbsp;Browser DevTools, Redux
-              DevTools, Postman,
-            </Typography>
-            <Typography
-              variant="subtitle1"
-              align="center"
-              style={{ color: "tan" }}
-            >
-              <strong>IDE:</strong>&nbsp;VS Code, WebStorm, Mongo Compass,
-              Postman
-            </Typography>
-            <Typography
-              variant="subtitle1"
-              align="center"
-              style={{ color: "tan" }}
-            >
-              <strong>Backend:</strong>&nbsp;NodeJs, Express
-            </Typography>
-            <Typography
-              variant="subtitle1"
-              align="center"
-              style={{ color: "tan" }}
-            >
-              <strong>Other:</strong>&nbsp;UmiJs, Git/GitHub , JIRA
-            </Typography>
-            <Typography
-              variant="subtitle1"
-              align="center"
-              style={{ color: "tan" }}
-            >
-              <strong>DBMS:</strong>&nbsp;MongoDB
-            </Typography>
-          </Box>
+      <Container component="div" align="center">
+        <Typography variant="h5" align="center" className={classes.subHeading}>
+          Online Resume
+        </Typography>
+        <Button
+          variant="contained"
+          className={classes.button}
+          href={MyResume}
+          download="Resume_Oleksandr_Dashevskyi"
+        >
+          Download PDF Version
+        </Button>
+      </Container>
+
+      <Container
+        component="div"
+        className={classes.mainContainer}
+        maxWidth="lg"
+      >
+        <Container component="div" className={classes.secondaryContainer}>
+          <Typography
+            variant="h5"
+            align="center"
+            style={{ color: "tomato", paddingTop: "1rem" }}
+          >
+            OLEKSANDR DASHEVSKYI
+          </Typography>
+          <Typography variant="h6" align="center" style={{ color: "tan" }}>
+            React Front End Web Developer
+          </Typography>
+          <Typography variant="body1" align="center" style={{ color: "tan" }}>
+            Irvine, CA 92603 | 310-600-4068
+          </Typography>
+        </Container>
+
+        <Container className={classes.secondaryContainer}>
+          <Typography variant="h5" align="center" style={{ color: "tomato" }}>
+            SUMMARY
+          </Typography>
+          <Typography
+            variant="body1"
+            align="left"
+            style={{ color: "tan" }}
+            gutterBottom
+          >
+            • React / JavaScript Developer with 3+ years of hands-on experience
+            in application development
+          </Typography>
+          <Typography
+            variant="body1"
+            align="left"
+            style={{ color: "tan" }}
+            gutterBottom
+          >
+            • Experience in all phases of software life cycle including
+            requirements gathering, project planning development
+          </Typography>
+          <Typography
+            variant="body1"
+            align="left"
+            style={{ color: "tan" }}
+            gutterBottom
+          >
+            • Strong knowledge of software development life cycle methodologies
+            such as Agile / SCRUM
+          </Typography>
+          <Typography variant="body1" align="left" style={{ color: "tan" }}>
+            • Experience with Git, continuous integration / development
+          </Typography>
+        </Container>
+
+        <Container className={classes.secondaryContainer}>
+          <Typography
+            variant="h5"
+            align="center"
+            style={{ color: "tomato" }}
+            gutterBottom
+          >
+            TECHNICAL SKILLS
+          </Typography>
+          <Typography
+            variant="body1"
+            align="left"
+            style={{ color: "tan" }}
+            gutterBottom
+          >
+            <strong>Languages:</strong>&nbsp;JavaScript (ES5, ES6), TypeScript,
+            HTML, CSS, JSON
+          </Typography>
+          <Typography
+            variant="body1"
+            align="left"
+            style={{ color: "tan" }}
+            gutterBottom
+          >
+            <strong>Libraries:</strong>&nbsp;React, Redux, Bootstrap, Ant
+            Design, Material UI
+          </Typography>
+          <Typography
+            variant="body1"
+            align="left"
+            style={{ color: "tan" }}
+            gutterBottom
+          >
+            <strong>Testing Tools:</strong>&nbsp;Browser DevTools, Redux
+            DevTools, Postman,
+          </Typography>
+          <Typography
+            variant="body1"
+            align="left"
+            style={{ color: "tan" }}
+            gutterBottom
+          >
+            <strong>IDE:</strong>&nbsp;VS Code, WebStorm, Mongo Compass, Postman
+          </Typography>
+          <Typography
+            variant="body1"
+            align="left"
+            style={{ color: "tan" }}
+            gutterBottom
+          >
+            <strong>Backend:</strong>&nbsp;NodeJs, Express
+          </Typography>
+          <Typography
+            variant="body1"
+            align="left"
+            style={{ color: "tan" }}
+            gutterBottom
+          >
+            <strong>Other:</strong>&nbsp;UmiJs, Git/GitHub , JIRA
+          </Typography>
+          <Typography variant="body1" align="left" style={{ color: "tan" }}>
+            <strong>DBMS:</strong>&nbsp;MongoDB
+          </Typography>
+        </Container>
+
+        <Container component="div">
           <Typography variant="h5" align="center" className={classes.heading}>
             WORK EXPERIENCE
           </Typography>
-
           <Box component="div" className={classes.timeLine}>
             <Typography
               variant="h3"
@@ -224,18 +302,19 @@ const Resume = () => {
               </Typography>
             </Box>
           </Box>
-          <Box component="div">
-            <Typography variant="h5" align="center" style={{ color: "tomato" }}>
-              EDUCATION
-            </Typography>
-            <Typography variant="body2" align="center" style={{ color: "tan" }}>
-              Kherson State Maritime Industry <br />
-              Bachelor’s degree of Navigation <br />
-              Vessel Navigator Computer technologies in Maritime industry <br />
-              2009-2011
-            </Typography>
-          </Box>
-        </Box>
+        </Container>
+
+        <Container component="div" className={classes.secondaryContainer}>
+          <Typography variant="h5" align="center" style={{ color: "tomato" }}>
+            EDUCATION
+          </Typography>
+          <Typography variant="body1" align="left" style={{ color: "tan", paddingBottom: "1rem"}}>
+            Kherson State Maritime Industry <br />
+            Bachelor’s degree of Naval Science / Computer technologies in
+            Maritime industry <br />
+            2011
+          </Typography>
+        </Container>
       </Container>
     </>
   );
